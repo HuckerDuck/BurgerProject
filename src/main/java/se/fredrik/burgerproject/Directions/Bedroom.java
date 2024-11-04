2efe94202598d5d62c21f9d2c2ab2b594a4822d5
Fredrik <frmb1@hotmail.com>
2024-11-04 15:37:14 +0100

    Nya saker är tillagda

--- src/main/java/se/fredrik/burgerproject/Directions/Bedroom.java
+++ src/main/java/se/fredrik/burgerproject/Directions/Bedroom.java
@@ -8,15 +8,21 @@ import se.fredrik.burgerproject.Tools.waitTimer;
 public class Bedroom implements RoomManager {
     @Override
     public void enter(Resident resident, Burglar burglar) {
+        System.out.println();
+        System.out.println(Colours.YELLOW);
         System.out.println("You enter the bedroom");
         System.out.println("The bed looks nice and comfy");
         System.out.println("You lie down and rest for a bit");
+        System.out.println();
         waitTimer.waitTimer(1000);
         System.out.println("Zzzz");
+        System.out.println();
         waitTimer.waitTimer(1000);
         System.out.println("Zzzz");
+        System.out.println();
         waitTimer.waitTimer(1000);
         System.out.println("Zzzz");
+        System.out.println();
         waitTimer.waitTimer(1000);
         System.out.println();
         System.out.println("You awake and go back to the Living Room");
